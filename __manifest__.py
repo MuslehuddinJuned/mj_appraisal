ce77dc62ded0569642bc81e04a53d1221e33b533
Muslehuddin Juned <41875815+MuslehuddinJuned@users.noreply.github.com>
2026-03-18 14:24:54 +0600

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    'name': 'Performance Appraisal',
+    'version': '19.0.1.0.0',
+    'category': 'Human Resources',
+    'summary': 'Manage employee performance appraisals',
+    'description': 'Module to manage performance appraisals for employees including appraisers and scoring.',
+    'author': 'Juned',
+    'website': 'http://www.zachai-bachhai.com',
+    'depends': ['base', 'hr', 'mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'reports/report.xml',
+        'reports/report_performance_appraisal.xml',
+        'views/performance_appraisal_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
